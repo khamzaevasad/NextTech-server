922241d762ce8fefdf693fe22989fb4b1cd012a5
khamzaevasad <xamzayevasad442@gmail.com>
2026-01-18 04:44:31 +0900

    feat: integrate @nestjs/config module and update environment variable handling

--- apps/next-tech-batch/src/main.ts
+++ apps/next-tech-batch/src/main.ts
@@ -3,6 +3,6 @@ import { NextTechBatchModule } from './next-tech-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NextTechBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/next-tech-batch/src/next-tech-batch.module.ts
+++ apps/next-tech-batch/src/next-tech-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NextTechBatchController } from './next-tech-batch.controller';
 import { NextTechBatchService } from './next-tech-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NextTechBatchController],
   providers: [NextTechBatchService],
 })
