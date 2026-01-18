662c89e48c9d0106e365d72833955e5b7536504e
khamzaevasad <xamzayevasad442@gmail.com>
2026-01-19 01:52:46 +0900

    feat: implement member resolver and service with signup and login mutations

--- apps/next-tech-api/src/components/member/member.resolver.ts
+++ apps/next-tech-api/src/components/member/member.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Resolver } from '@nestjs/graphql';
+import { MemberService } from './member.service';
 
 @Resolver()
-export class MemberResolver {}
+export class MemberResolver {
+  constructor(private readonly memberService: MemberService) {}
+
+  @Mutation(() => String)
+  //   signup
+  public async signup(): Promise<String> {
+    console.log('Mutation: Signup');
+    return await this.memberService.signup();
+  }
+
+  @Mutation(() => String)
+  //   login
+  public async login(): Promise<String> {
+    console.log('Mutation: login');
+    return await this.memberService.login();
+  }
+}
--- apps/next-tech-api/src/components/member/member.service.ts
+++ apps/next-tech-api/src/components/member/member.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  // signup
+  public async signup(): Promise<String> {
+    return 'signup executed';
+  }
+
+  // signup
+  public async login(): Promise<String> {
+    return 'login executed';
+  }
+}
