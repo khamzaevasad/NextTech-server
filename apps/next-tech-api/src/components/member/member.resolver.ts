86879bd4ab5893328c880f39eb5da603198ce6ab
khamzaevasad <xamzayevasad442@gmail.com>
2026-01-20 16:37:41 +0900

    feat: refine member retrieval and update methods with improved type handling and query logic

--- apps/next-tech-api/src/components/member/member.resolver.ts
+++ apps/next-tech-api/src/components/member/member.resolver.ts
@@ -10,6 +10,7 @@ import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import type { ObjectId } from 'mongoose';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class MemberResolver {
@@ -48,11 +49,12 @@ export class MemberResolver {
     return await this.memberService.updateMember(memberId, input);
   }
 
-  @Query(() => String)
+  @Query(() => Member)
   // getMember
-  public async getMember(): Promise<String> {
+  public async getMember(@Args('memberId') input: string): Promise<Member> {
     console.log('Query: getMember');
-    return await this.memberService.getMember();
+    const targetId = shapeIntoMongoObjectId(input);
+    return await this.memberService.getMember(targetId);
   }
 
   /**ADMIN**/
