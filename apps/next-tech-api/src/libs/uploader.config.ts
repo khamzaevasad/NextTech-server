21cb967890cb048c572b0a67c52e29e8f183c3b1
khamzaevasad <xamzayevasad442@gmail.com>
2026-01-24 09:30:00 +0900

    feat: add image uploader mutations and uploader configuration for file handling

---
+++ apps/next-tech-api/src/libs/uploader.config.ts
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+  const ext = path.parse(filename).ext;
+  return uuidv4() + ext;
+};
