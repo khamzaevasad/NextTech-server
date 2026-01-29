4a2f45d788191e5a32327ccc13532fe72c8f08fd
khamzaevasad <xamzayevasad442@gmail.com>
2026-01-29 21:48:05 +0900

    feat: add lookupAuthMemberFollowed function to enhance follow data retrieval

--- apps/next-tech-api/src/components/follow/follow.service.ts
+++ apps/next-tech-api/src/components/follow/follow.service.ts
@@ -6,7 +6,12 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import {
+  lookupAuthMemberFollowed,
+  lookupAuthMemberLiked,
+  lookupFollowerData,
+  lookupFollowingData,
+} from '../../libs/config';
 
 @Injectable()
 export class FollowService {
@@ -93,8 +98,7 @@ export class FollowService {
             list: [
               { $skip: (page - 1) * limit },
               { $limit: limit },
-              // lookupAuthMemberLiked(memberId, '$followingId'),
-              //   lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followingId' }),
+              lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followingId' }),
               lookupFollowingData,
               { $unwind: '$followingData' },
             ],
@@ -126,8 +130,7 @@ export class FollowService {
             list: [
               { $skip: (page - 1) * limit },
               { $limit: limit },
-              //   lookupAuthMemberLiked(memberId, '$followerId'),
-              //   lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followerId' }),
+              lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followerId' }),
               lookupFollowerData,
               { $unwind: '$followerData' },
             ],
