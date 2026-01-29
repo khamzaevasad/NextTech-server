a3e19234412800f1b79482103ed338c8af3d2fc2
khamzaevasad <xamzayevasad442@gmail.com>
2026-01-29 13:28:27 +0900

    feat: update Follow module and service to include StoreModule and enhance FollowService constructor

--- apps/next-tech-api/src/components/follow/follow.module.ts
+++ apps/next-tech-api/src/components/follow/follow.module.ts
@@ -3,9 +3,16 @@ import { FollowService } from './follow.service';
 import { FollowResolver } from './follow.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import FollowSchema from '../../schemas/Follow.model';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { StoreModule } from '../store/store.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Follow', schema: FollowSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Follow', schema: FollowSchema }]),
+    AuthModule,
+    MemberModule,
+  ],
   providers: [FollowService, FollowResolver],
   exports: [FollowService],
 })
