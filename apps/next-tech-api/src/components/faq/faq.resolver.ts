0c8a1895434371c4f9961b79ae1344d2ae0c6ade
khamzaevasad <xamzayevasad442@gmail.com>
2026-02-24 21:18:54 +0900

    feat: add getFaq query and service method for FAQ retrieval

--- apps/next-tech-api/src/components/faq/faq.resolver.ts
+++ apps/next-tech-api/src/components/faq/faq.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { FaqService } from './faq.service';
 import { UseGuards } from '@nestjs/common';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -8,6 +8,8 @@ import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { FaqInput } from '../../libs/dto/faq/faq.input';
 import type { ObjectId } from 'mongoose';
 import { Faq } from '../../libs/dto/faq/faq';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class FaqResolver {
@@ -16,11 +18,22 @@ export class FaqResolver {
   @UseGuards(RolesGuard)
   @Roles(MemberType.ADMIN)
   @Mutation(() => Faq)
-  /* ------------------------------ createNotice ------------------------------ */
+  /* ------------------------------ createFaq ------------------------------ */
   public async createFaq(
     @Args('input') input: FaqInput,
     @AuthMember('_id') memberId: ObjectId,
   ): Promise<Faq> {
     return await this.faqService.createFaq(memberId, input);
+  }
+
+  @UseGuards(WithoutGuard)
+  @Query(() => Faq)
+  /* -------------------------------- getFaq ------------------------------- */
+  public async getFaq(
+    @Args('faqId') input: string,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Faq> {
+    const faqId = shapeIntoMongoObjectId(input);
+    return await this.faqService.getFaq(faqId);
   }
 }
--- apps/next-tech-api/src/components/faq/faq.service.ts
+++ apps/next-tech-api/src/components/faq/faq.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Faq } from '../../libs/dto/faq/faq';
 import type { ObjectId } from 'mongoose';
 import { FaqInput } from '../../libs/dto/faq/faq.input';
 import { Message } from '../../libs/enums/common.enum';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class FaqService {
@@ -20,5 +21,12 @@ export class FaqService {
       console.log('Error ServiceModel', err);
       throw new BadRequestException(Message.CREATE_FAILED);
     }
+  }
+
+  /* -------------------------------- getFaq ------------------------------- */
+  public async getFaq(faqId: ObjectId): Promise<Faq> {
+    const result: Faq | null = await this.faqModel.findById({ _id: faqId }).exec();
+    if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+    return result;
   }
 }
